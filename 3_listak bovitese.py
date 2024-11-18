11816e9aa95b10c0e9b6c33f06c3dbd028a682e4
Szabi1234234 <toth.szabolcs1@diak.szbi-pg.hu>
2024-11-18 08:40:42 +0100

    add list methods

---
+++ 3_listak bovitese.py
@@ -0,0 +1,21 @@
+nyelvek = ["python " "python " "C " "C++ " "java"]
+
+
+# a lista végére hozzáfűz egy elemet
+nyelvek.append('Python')
+   
+# a listát másolja
+nyelvek_masolat = nyelvek.copy()
+   
+# a lista végére hozzáfűz egy másik gyűjteményt (pl. listát)
+nyelvek_honlapkesziteshez = ['HTML', 'CSS', 'JavaScript']
+nyelvek.extend(nyelvek_honlapkesziteshez)
+print(nyelvek)
+   
+# adott indexű helyre beszúrja a megadott elemet
+nyelvek.insert(1, 'Go')   
+print(nyelvek)  
+print(len(nyelvek))
+  
+
+
